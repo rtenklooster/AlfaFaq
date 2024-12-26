8a116c1696c78be9843faae5c891f9226652c5f8
Richard ten Klooster <richard@MacBook-Pro-van-Richard-2.local>
2024-12-26 22:55:35 +0100

    Clean repository and remove sensitive data

--- lib/utils/pnpjs-config.d.ts
+++
@@ -1,8 +0,0 @@
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { SPFI } from "@pnp/sp";
-import "@pnp/sp/webs";
-import "@pnp/sp/lists";
-import "@pnp/sp/items";
-import "@pnp/sp/batching";
-import "@pnp/sp/fields";
-export declare const getSP: (context?: WebPartContext) => SPFI;
